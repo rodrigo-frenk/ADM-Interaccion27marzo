fa7a0eee0f052d2372c6598f6a60c9c93c7cd285
rodrigo-frenk <rodrigo.frenk@gmail.com>
2017-04-03 21:42:57 -0500

    maquetado inicial listo, basicos de jquery

--- intro_ux/01-registro.md
+++ intro_ux/01-registro.md
@@ -3,20 +3,31 @@
 Como Visitante, quiero crear una Cuenta para participar en la Plataforma.
 
 
-1. Ingresar a Plataforma
+# Plantillas:
 
-Landing Page: Página de Aterrizaje
-   - Cabecera
-      - Logo
-      - Botones Usuarios
-         - Botón Registro
-         - Botón Ingreso
-   - Hero Image (Imagen destacada)
-   - Título
-   - Subtítulo
-   - Llamado a la Acción: Registrarse
-      - Enunciado de invitación
-      - Botón      
+### Plantilla General
+
+- Cabecera
+   - Logo
+   - Botones Usuarios
+      - Botón Registro
+      - Botón Ingreso
+
+- Pie de Página
+   - Info de Copyright
+   - Enlaces a 3 redes sociales
+
+### Página de Inicio
+
+
+
+   - Portada
+      - Hero Image (Imagen destacada)
+      - Título
+      - Subtítulo
+      - Llamado a la Acción: Registrarse
+         - Enunciado de invitación
+         - Botón      
    - 3 Puntos principales
       - Imagen
       - Titular
@@ -27,3 +38,5 @@ Landing Page: Página de Aterrizaje
       - Título
       - Texto de invitación
       - Botón enlace a Página "Registro"
+
+### Registro
--- intro_ux/landing/js/landing.js
+++ intro_ux/landing/js/landing.js
@@ -3,6 +3,41 @@ $(document).ready(function(){
    // Escala y Recorta la imagen para que quepa exacto en su contenedor
    $('.imgLiquidFill').imgLiquid()
 
+   // Configuramos nuestro plugin a partir de un Objeto de JS
+   $('.imgLiquidNoFill').imgLiquid({
+      fill: false
+   })
+
+
+   // Centrado Vertical:
+   // Iterar por cada elemento que tenga la clase 'v-center'
+   $('.v-center').each(function(){
+
+      elementoPadre = $(this)
+
+      elementosHijos = elementoPadre.children()
+
+      alturaHijos = 0
+
+      // ahora, iterar por cada uno de estos elementos hijos:
+      elementosHijos.each(function(){
+         elementoHijo = $(this)
+         // sumar la altura del elemento hijo a la altura de todos los hijos en conjunto
+         alturaHijos += elementoHijo.height()
+      })
+
+      diferencia = elementoPadre.height() - alturaHijos
+      // dividir diferencia entre 2
+      espacioSuperior = diferencia / 2
+
+      // dar espaciado superior al padre, para que empuje sus hijos hacia abajo
+      elementoPadre.css({
+         paddingTop: espacioSuperior
+      })
+
+   })
+
+
    console.log('Landing Lista')
 
 })
